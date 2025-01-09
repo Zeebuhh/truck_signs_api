c84c5dba163a808045acb6cd63fa1ecc60fba60c
Zeebuhh <ngroth277@gmail.com>
2025-01-09 22:17:04 +0100

    added fake dummy migration

---
+++ backend/migrations/0018_auto_20250109_2058.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('backend', '0017_auto_20210724_2304'),
+    ]
+
+    operations = [
+        # Keine Operationen, da wir 'nur' die Migration an den DB-Stand anpassen
+    ]
